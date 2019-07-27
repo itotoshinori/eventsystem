7c2690c9bc91d64d87d0f43143ea49f88164845c
Your Name <your.email@example.com>
2019-07-27 02:54:10 +0000

    メールシステム89

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -101,15 +101,15 @@ class EventsController < ApplicationController
     if @kubun=="1"
       sanka=Participant.new(user_id:user_id,event_id:@event.id)
       sanka.save
-      @link="https://young-gorge-92470.herokuapp.com/events/#{@event.id}"
+      @link="https://young-gorge-92470.herokuapp.com/events/#{sanka.event_id}"
       @content="開催イベント：#{@event.title}に#{usernamereturn(user_id)}さんが参加登録されました"
       @user=User.where(id:@event.user_id)
       sendmailsys
       flash[:success]="正常に参加登録されました"
     elsif @kubun=="2"
       sanka=@participant=Participant.find_by(event_id: @event.id.to_i, user_id: current_user.id)
+      @link="https://young-gorge-92470.herokuapp.com/events/#{sanka.event_id}"
       sanka.destroy
-      @link="https://young-gorge-92470.herokuapp.com/events/#{@event.id}"
       @content="開催イベント：#{@event.title}に#{usernamereturn(user_id)}さんがキャンセル登録されました"
       @user=User.where(id:@event.user_id)
       sendmailsys
