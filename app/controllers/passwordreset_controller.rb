96d3b4f226f84ea3496a907774e35e65632b2261
Your Name <your.email@example.com>
2019-08-21 19:49:54 +0000

    passrest2

--- app/controllers/passwordreset_controller.rb
+++ app/controllers/passwordreset_controller.rb
@@ -9,12 +9,11 @@ class PasswordresetController < ApplicationController
       user_id=@user.id
       passnum=SecureRandom.hex(8)
       passwordreset=Passwordreset.new(passnum:passnum,user_id:user_id,email:email)
-      #passwordreset.save
+      passwordreset.save
       @content="下記アドレスをクリックしてパスワードをリセットして下さい"
-      @link="http://yahoo.co.jp"
+      @link="https://enigmatic-lowlands-69028.herokuapp.com/passworrest/#{passnum}/#{passwordreset.id}"
       MailsysMailer.sendmail(@content,@link,email).deliver_later
-      #MailsysMailer.sendmail(@content,@link,u.email).deliver_later
-      flash[:success]="パスワードのリセットのためのメールを送りました#{email}"
+      flash[:success]="パスワードのリセットのためのメールを送りました"
     else
       flash[:notice]="登録のアドレスではありません。処理を中止しました。"
     end
