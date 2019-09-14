4f3bb9d76dc230718440c96f0876e809ef01b6b4
Your Name <your.email@example.com>
2019-09-14 07:32:03 +0000

    clone2

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,7 +6,7 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
-  URL="https://enigmatic-lowlands-69028.herokuapp.com/"
+  URL="https://young-gorge-92470.herokuapp.com/"
   protected
 
     def configure_permitted_parameters
