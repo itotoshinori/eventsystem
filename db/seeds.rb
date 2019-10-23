8835658a1468824cd4d067d827d64b215297bd90
Your Name <your.email@example.com>
2019-10-23 11:39:36 +0000

    port-111

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+User.create!( name:"中原一馬",
+             email: "tito40358@outlook.jp",
+             password:"password1234")
