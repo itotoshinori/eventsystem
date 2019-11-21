729811fabe16f4c1387a4aa6abc4509cc2dd712c
Your Name <your.email@example.com>
2019-11-21 05:23:21 +0000

    cal17

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -263,12 +263,12 @@ class EventsController < ApplicationController
         @pts << Ptcollection.new(f.user_id.to_s,usernamereturn(f.user.id)+"さんに送る")
     end
     @userplus=Comment.joins(:user).where(event_id:@event.id).where.not(user_id: @event.user_id).group("user_id")
-    #@userplus.each do |f|
-      #if @user.where(user_id:f.user_id).empty?
-        #@pts << Ptcollection.new(f.user_id,usernamereturn(f.user.id)+"さんに送る")
+    @userplus.each do |f|
+      if @user.where(user_id:f.user_id).empty?
+        @pts << Ptcollection.new(f.user_id,usernamereturn(f.user.id)+"さんに送る")
         #@user.new(user_id:f.user_id)
-      #end
-    #end
+      end
+    end
   end
   
   def settingvalue
